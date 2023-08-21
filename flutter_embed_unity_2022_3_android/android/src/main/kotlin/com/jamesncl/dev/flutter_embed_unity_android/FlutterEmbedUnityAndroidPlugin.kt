517cc4537be0ffae59d4f6070ad0cebdc2d5274e
jamesncl <jamesncl@users.noreply.github.com>
2023-08-21 10:50:35 +0100

    Informative error screens if platform view factory fails

--- flutter_embed_unity_2022_3_android/android/src/main/kotlin/com/jamesncl/dev/flutter_embed_unity_android/FlutterEmbedUnityAndroidPlugin.kt
+++ flutter_embed_unity_2022_3_android/android/src/main/kotlin/com/jamesncl/dev/flutter_embed_unity_android/FlutterEmbedUnityAndroidPlugin.kt
@@ -15,86 +15,91 @@ import io.flutter.Log
  * See https://docs.flutter.dev/release/breaking-changes/plugin-api-migration#uiactivity-plugin
  * and https://api.flutter.dev/javadoc/io/flutter/embedding/engine/plugins/activity/ActivityAware.html
  * */
-class FlutterEmbedUnityAndroidPlugin: FlutterPlugin, ActivityAware {
-  // The MethodChannel that will the communication between Flutter and native Android
-  private lateinit var channel : MethodChannel
-  // This is a simple reference holder for the currently active PlatformView, which we can pass to
-  // the method call handler (so method calls can interact with the UnityPlayer) and to
-  // the PlatformViewFactory (which will update the reference when a new PlatformView is created)
-  private val platformViewRegistry = PlatformViewRegistry()
-  private val flutterActivityRegistry = FlutterActivityRegistry()
-  private val methodCallHandler = FlutterMethodCallHandler(platformViewRegistry)
+class FlutterEmbedUnityAndroidPlugin : FlutterPlugin, ActivityAware {
+    // The MethodChannel that will the communication between Flutter and native Android
+    private lateinit var channel: MethodChannel
 
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    Log.d(logTag, "onAttachedToEngine")
-    // Set up the method channel between flutter and android
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, uniqueIdentifier)
-    channel.setMethodCallHandler(methodCallHandler)
-    // Register the method channel with SendToFlutter static class (which is called by Unity)
-    // so messages from Unity can be forwarded on to Flutter
-    SendToFlutter.methodChannel = channel
+    // This is a simple reference holder for the currently active PlatformView, which we can pass to
+    // the method call handler (so method calls can interact with the UnityPlayer) and to
+    // the PlatformViewFactory (which will update the reference when a new PlatformView is created)
+    private val platformViewRegistry = PlatformViewRegistry()
+    private val flutterActivityRegistry = FlutterActivityRegistry()
+    private val methodCallHandler = FlutterMethodCallHandler(platformViewRegistry)
 
-    // Register a view factory
-    // On the Flutter side, when we create a PlatformView with our unique identifier:
-    // AndroidView(
-    //    viewType: Constants.uniqueViewIdentifier,
-    // )
-    //
-    // the UnityViewFactory will be invoked to create a UnityPlatformView:
-    flutterPluginBinding
-      .platformViewRegistry
-      .registerViewFactory(
-        uniqueIdentifier,
-        // The factory needs the activity which will be received in onAttachedToActivity
-        // so that the UnityPlayer can be created. It also needs to be able to update the
-        // platformViewRegistry with the current PlatformView each time a PlatformView is created
-        UnityViewFactory(flutterActivityRegistry, platformViewRegistry)
-      )
-  }
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        Log.d(logTag, "onAttachedToEngine")
+        // Set up the method channel between flutter and android
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, uniqueIdentifier)
+        channel.setMethodCallHandler(methodCallHandler)
+        // Register the method channel with SendToFlutter static class (which is called by Unity)
+        // so messages from Unity can be forwarded on to Flutter
+        SendToFlutter.methodChannel = channel
+
+        // Register a view factory
+        // On the Flutter side, when we create a PlatformView with our unique identifier:
+        // AndroidView(
+        //    viewType: Constants.uniqueViewIdentifier,
+        // )
+        //
+        // the UnityViewFactory will be invoked to create a UnityPlatformView:
+        flutterPluginBinding
+            .platformViewRegistry
+            .registerViewFactory(
+                uniqueIdentifier,
+                // The factory needs the activity which will be received in onAttachedToActivity
+                // so that the UnityPlayer can be created. It also needs to be able to update the
+                // platformViewRegistry with the current PlatformView each time a PlatformView is created
+                UnityViewFactory(flutterActivityRegistry, platformViewRegistry)
+            )
+    }
 
 
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    Log.d(logTag, "onDetachedFromEngine")
-    channel.setMethodCallHandler(null)
-  }
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        Log.d(logTag, "onDetachedFromEngine")
+        channel.setMethodCallHandler(null)
+    }
 
-  // ActivityAware
-  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    Log.d(logTag, "onAttachedToActivity")
-    flutterActivityRegistry.activity = binding.activity
-  }
+    // ActivityAware
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        Log.d(logTag, "onAttachedToActivity")
+        flutterActivityRegistry.activity = binding.activity
+    }
 
-  // ActivityAware
-  override fun onDetachedFromActivityForConfigChanges() {
-    Log.w(logTag, "onDetachedFromActivityForConfigChanges - this means the Flutter activity " +
-            "for your app was destroyed to process a configuration change. This scenario is not supported " +
-            "and may lead to unexpected behaviour or crashes. You may be able to prevent configuration " +
-            "changes causing the activity to be destroyed by adding values to the android:configChanges " +
-            "attribute for your main activity in your app's AndroidManifest.xml. For example, if this " +
-            "happened on orientation change, add orientation|keyboardHidden|screenSize|screenLayout to " +
-            "android:configChanges. See " +
-            "https://developer.android.com/guide/topics/resources/runtime-changes#restrict-activity " +
-            "for more information")
-    // This will be called if the Activity is destroyed / recreated due to a 'configuration change'.
-    // Not sure if this can be handled at all - UnityPlayer is designed to only run in it's own
-    // activity, on a separate process, and not to be reused
-    // See https://docs.unity3d.com/Manual/UnityasaLibrary-Android.html
-    // TODO(Is there any way to handle FlutterActivity onDestroy()?)
+    // ActivityAware
+    override fun onDetachedFromActivityForConfigChanges() {
+        Log.w(
+            logTag, "onDetachedFromActivityForConfigChanges - this means the Flutter activity " +
+                    "for your app was destroyed to process a configuration change. This scenario is not supported " +
+                    "and may lead to unexpected behaviour or crashes. You may be able to prevent configuration " +
+                    "changes causing the activity to be destroyed by adding values to the android:configChanges " +
+                    "attribute for your main activity in your app's AndroidManifest.xml. For example, if this " +
+                    "happened on orientation change, add orientation|keyboardHidden|screenSize|screenLayout to " +
+                    "android:configChanges. See " +
+                    "https://developer.android.com/guide/topics/resources/runtime-changes#restrict-activity " +
+                    "for more information"
+        )
+        // This will be called if the Activity is destroyed / recreated due to a 'configuration change'.
+        // Not sure if this can be handled at all - UnityPlayer is designed to only run in it's own
+        // activity, on a separate process, and not to be reused
+        // See https://docs.unity3d.com/Manual/UnityasaLibrary-Android.html
+        // TODO(Is there any way to handle FlutterActivity onDestroy()?)
 
-    flutterActivityRegistry.activity = null
-  }
+        flutterActivityRegistry.activity = null
+    }
 
-  // ActivityAware
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    Log.d(logTag, "onReattachedToActivityForConfigChanges")
-    flutterActivityRegistry.activity = binding.activity
-  }
+    // ActivityAware
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        Log.d(logTag, "onReattachedToActivityForConfigChanges")
+        flutterActivityRegistry.activity = binding.activity
+    }
 
-  // ActivityAware
-  override fun onDetachedFromActivity() {
-    Log.w(logTag, "onDetachedFromActivity - this means the Flutter activity " +
-            "for your app was destroyed. This scenario is not supported ")
-    // TODO(Is there any way to handle FlutterActivity onDestroy()?)
-    flutterActivityRegistry.activity = null
-  }
+    // ActivityAware
+    override fun onDetachedFromActivity() {
+        Log.w(
+            logTag, "onDetachedFromActivity - this means the Flutter activity " +
+                    "for your app was destroyed. This scenario is not supported "
+        )
+        // TODO(Is there any way to handle FlutterActivity onDestroy()?)
+        flutterActivityRegistry.activity = null
+    }
 }
--- flutter_embed_unity_2022_3_android/android/src/main/kotlin/com/jamesncl/dev/flutter_embed_unity_android/view/UnityViewFactory.kt
+++ flutter_embed_unity_2022_3_android/android/src/main/kotlin/com/jamesncl/dev/flutter_embed_unity_android/view/UnityViewFactory.kt
@@ -1,7 +1,14 @@
 package com.jamesncl.dev.flutter_embed_unity_android.view
 
 import android.content.Context
+import android.graphics.Color
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.TextView
+import com.jamesncl.dev.flutter_embed_unity_android.BuildConfig
 import com.jamesncl.dev.flutter_embed_unity_android.FlutterActivityRegistry
 import com.jamesncl.dev.flutter_embed_unity_android.FlutterEmbedConstants.Companion.logTag
 import io.flutter.Log
@@ -16,24 +23,60 @@ class UnityViewFactory(
     override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
         Log.d(this.toString(), "UnityViewFactory creating view $viewId")
 
-        // UnityPlayer expects to be passed a context which is an Activity. It will crash during
-        // resume() if it isn't (NullPointerException: Attempt to invoke virtual method
-        // 'android.content.ContentResolver android.content.Context.getContentResolver()' on a null
-        // object reference)
-        flutterActivityRegistry.activity.let { activity ->
-            if (activity != null) {
-                val unityPlayerCustom = UnityPlayerCustom.getInstance(activity)
-                val unityPlatformView = UnityPlatformView(unityPlayerCustom, context)
-                platformViewRegistry.activePlatformView = unityPlatformView
-                return unityPlatformView
-            }
-            else {
-                Log.e(logTag, "Error creating UnityPlatformView: an activity is not available")
-                return object : PlatformView {
-                    override fun getView(): View? { return null }
-                    override fun dispose() {}
+        try {
+            // UnityPlayer expects to be passed a context which is an Activity. It will crash during
+            // resume() if it isn't (NullPointerException: Attempt to invoke virtual method
+            // 'android.content.ContentResolver android.content.Context.getContentResolver()' on a null
+            // object reference)
+            flutterActivityRegistry.activity.let { activity ->
+                if (activity != null) {
+                    val unityPlayerCustom = UnityPlayerCustom.getInstance(activity)
+                    val unityPlatformView = UnityPlatformView(unityPlayerCustom, context)
+                    platformViewRegistry.activePlatformView = unityPlatformView
+                    return unityPlatformView
+                }
+                else {
+                    val errorMessage = "Error creating UnityPlatformView: an activity is not available"
+                    Log.e(logTag, errorMessage)
+                    return createErrorView(context, errorMessage)
                 }
             }
+        }
+        catch (e: NoClassDefFoundError) {
+            val errorMessage = "Unity library not found. Your exported Unity " +
+                    "project is not correctly linked to your Android app. Make sure " +
+                    "you have followed the required setup steps in the " +
+                    "flutter_embed_unity README, in particular the modifications to " +
+                    "your project's settings.gradle, properites.gradle, and your " +
+                    "app module's build.gradle."
+            Log.e(logTag, errorMessage)
+            return createErrorView(context, errorMessage)
+        }
+        catch(e: Throwable) {
+            val errorMessage = e.message ?: e.toString()
+            Log.e(logTag, errorMessage)
+            return createErrorView(context, errorMessage)
+        }
+    }
+
+    private fun createErrorView(context: Context, errorMessage: String) : PlatformView {
+        return object : PlatformView {
+            override fun getView(): View {
+                val layout = RelativeLayout(context)
+                layout.setBackgroundColor(Color.parseColor("#FFFB49"))
+                if(BuildConfig.DEBUG) {
+                    val textView = TextView(context)
+                    textView.text = errorMessage
+                    textView.setTextColor(Color.parseColor("#000000"))
+                    val params: RelativeLayout.LayoutParams =
+                        RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                    params.addRule(RelativeLayout.CENTER_IN_PARENT)
+                    textView.layoutParams = params
+                    layout.addView(textView)
+                }
+                return layout
+            }
+            override fun dispose() {}
         }
     }
 }
