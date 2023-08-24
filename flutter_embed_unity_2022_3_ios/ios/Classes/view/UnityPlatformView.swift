035fefda1864f798e48d83ea4918bcea03d31525
James <James@james.james>
2023-08-24 11:26:08 +0100

    Remove old code

--- flutter_embed_unity_2022_3_ios/ios/Classes/view/UnityPlatformView.swift
+++ flutter_embed_unity_2022_3_ios/ios/Classes/view/UnityPlatformView.swift
@@ -5,8 +5,6 @@ import UnityFramework
 class UnityPlatformView : NSObject, FlutterPlatformView {
     
     private let unityAppController: UnityAppController
-//    private let _view: UIView
-    
     
     init(
         frame: CGRect,
@@ -15,23 +13,10 @@ class UnityPlatformView : NSObject, FlutterPlatformView {
         binaryMessenger messenger: FlutterBinaryMessenger?
     ) {
         self.unityAppController = UnityPlayerCustom.getInstance().appController()
-//        _view = UIView()
         super.init()
     }
 
     func view() -> UIView {
         return unityAppController.rootView
     }
-    
-    
-
-//    func createNativeView(view _view: UIView){
-//        _view.backgroundColor = UIColor.blue
-//        let nativeLabel = UILabel()
-//        nativeLabel.text = "Native text from iOS"
-//        nativeLabel.textColor = UIColor.white
-//        nativeLabel.textAlignment = .center
-//        nativeLabel.frame = CGRect(x: 0, y: 0, width: 180, height: 48.0)
-//        _view.addSubview(nativeLabel)
-//    }
 }
