021571d52825e49b533e83fe1d20ee2307a56f00
jamesncl <jamesncl@users.noreply.github.com>
2023-09-01 14:38:52 +0100

    Simplified example

--- flutter_embed_unity_2022_3_android/example/lib/main.dart
+++ flutter_embed_unity_2022_3_android/example/lib/main.dart
@@ -1,52 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_embed_unity/flutter_embed_unity.dart';
 import 'package:intl/intl.dart';
-import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ExampleApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class ExampleApp extends StatefulWidget {
+  const ExampleApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<ExampleApp> createState() => _ExampleAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider<AppState>(
-      create: (_) => AppState(),
-      child: MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Plugin example app'),
-          ),
-          body: Builder(
-            builder: (context) => Center(
-              child: ElevatedButton(
-                child: const Text("Open unity screen"),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const UnityScreen()
-                      )
-                  );
-                },
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-
-class UnityScreen extends StatelessWidget {
+class _ExampleAppState extends State<ExampleApp> {
 
   // When converting between strings and numbers in a message protocol
   // always use a fixed locale, to prevent unexpected parsing errors when
@@ -57,130 +24,143 @@ class UnityScreen extends StatelessWidget {
     decimalDigits: 2,
   );
 
-  const UnityScreen();
+  bool? _isUnityArSupportedOnDevice;
+  bool _isArSceneActive = false;
+  double _rotationSpeed = 30;
+  int _numberOfTaps = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<AppState>(
-      builder: (context, appState, child) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Builder(
+          builder: (context) {
+            final theme = Theme.of(context);
+            final bool? isUnityArSupportedOnDevice = _isUnityArSupportedOnDevice;
+            final String arStatusMessage;
 
-        final theme = Theme.of(context);
-        final bool? isUnityArSupportedOnDevice = appState.isUnityArSupportedOnDevice;
-        final String arStatusMessage;
+            if(isUnityArSupportedOnDevice == null) {
+              arStatusMessage = "checking...";
+            }
+            else if(isUnityArSupportedOnDevice) {
+              arStatusMessage = "supported";
+            }
+            else {
+              arStatusMessage = "not supported on this device";
+            }
 
-        if(isUnityArSupportedOnDevice == null) {
-          arStatusMessage = "checking...";
-        }
-        else if(isUnityArSupportedOnDevice) {
-          arStatusMessage = "supported";
-        }
-        else {
-          arStatusMessage = "not supported on this device";
-        }
-
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Unity'),
-          ),
-          body: Column(
-            children: [
-              Expanded(
-                child: EmbedUnity(
-                  onMessageFromUnity: (String data) {
-                    debugPrint("onMessageFromUnity $data");
-                    if(data == "touch"){
-                      appState.update(numberOfTaps: appState.numberOfTaps + 1);
-                    }
-                    else if(data == "scene_loaded") {
-                      _sendRotationSpeedToUnity(appState.rotationSpeed);
-                    }
-                    else if(data == "ar:true") {
-                      appState.update(isUnityArSupportedOnDevice: true);
-                    }
-                    else if(data == "ar:false") {
-                      appState.update(isUnityArSupportedOnDevice: false);
-                    }
-                  },
+            return Column(
+              children: [
+                Expanded(
+                  child: EmbedUnity(
+                    onMessageFromUnity: (String data) {
+                      // A message has been received from a Unity script
+                      if(data == "touch"){
+                        setState(() {
+                          _numberOfTaps = _numberOfTaps++;
+                        });
+                      }
+                      else if(data == "scene_loaded") {
+                        _sendRotationSpeedToUnity(_rotationSpeed);
+                      }
+                      else if(data == "ar:true") {
+                        setState(() {
+                          _isUnityArSupportedOnDevice = true;
+                        });
+                      }
+                      else if(data == "ar:false") {
+                        setState(() {
+                          _isUnityArSupportedOnDevice = false;
+                        });
+                      }
+                    },
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16),
-                child: Text(
-                  "Flutter logo has been touched ${appState.numberOfTaps} times",
-                  textAlign: TextAlign.center,
-                  style: theme.textTheme.titleMedium,
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Text(
+                    "Flutter logo has been touched $_numberOfTaps times",
+                    textAlign: TextAlign.center,
+                    style: theme.textTheme.titleMedium,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Row(
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
+                    children: [
+                      Text("Activate AR ($arStatusMessage)"),
+                      Switch(
+                        value: _isArSceneActive,
+                        onChanged: isUnityArSupportedOnDevice != null && isUnityArSupportedOnDevice ? (value) {
+                          sendToUnity(
+                            "SceneSwitcher",
+                            "SwitchToScene",
+                            _isArSceneActive ? "FlutterEmbedExampleScene" : "FlutterEmbedExampleSceneAR",
+                          );
+                          setState(() {
+                            _isArSceneActive = value;
+                          });
+                        } : null,
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
                   children: [
-                    Text("Activate AR ($arStatusMessage)"),
-                    Switch(
-                      value: appState.isArSceneActive,
-                      onChanged: isUnityArSupportedOnDevice != null && isUnityArSupportedOnDevice ? (value) {
-                        sendToUnity(
-                          "SceneSwitcher",
-                          "SwitchToScene",
-                          appState.isArSceneActive ? "FlutterEmbedExampleScene" : "FlutterEmbedExampleSceneAR",
-                        );
-                        appState.update(isArSceneActive: value);
-                      } : null,
+                    const Padding(
+                      padding: EdgeInsets.only(left: 16),
+                      child: Text(
+                        "Speed",
+                      ),
+                    ),
+                    Expanded(
+                      child: Slider(
+                        min: -200,
+                        max: 200,
+                        value: _rotationSpeed,
+                        onChanged: (value) {
+                          setState(() {
+                            _rotationSpeed = value;
+                          });
+                          _sendRotationSpeedToUnity(value);
+                        },
+                      ),
                     ),
                   ],
                 ),
-              ),
-              Row(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(left: 16),
-                    child: Text(
-                      "Speed",
-                    ),
-                  ),
-                  Expanded(
-                    child: Slider(
-                      min: -200,
-                      max: 200,
-                      value: appState.rotationSpeed,
-                      onChanged: (value) {
-                        appState.update(rotationSpeed: value);
-                        _sendRotationSpeedToUnity(value);
-                      },
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          pauseUnity();
-                        },
-                        child: const Text("Pause"),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            pauseUnity();
+                          },
+                          child: const Text("Pause"),
+                        ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          resumeUnity();
-                        },
-                        child: const Text("Resume"),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            resumeUnity();
+                          },
+                          child: const Text("Resume"),
+                        ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          showDialog(context: context, builder: (context) =>
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            showDialog(context: context, builder: (context) =>
                             const AlertDialog(
                               content: Column(
                                 mainAxisSize: MainAxisSize.min,
@@ -192,25 +172,26 @@ class UnityScreen extends StatelessWidget {
                                   ),
                                   Text(
                                     "Unity can only be shown in 1 widget at a time. If a new route "
-                                    "with a FlutterEmbed is pushed onto the stack, the one underneath is "
-                                    "'detached' from Unity, and restored when the route is popped",
+                                        "with a FlutterEmbed is pushed onto the stack, the one underneath is "
+                                        "'detached' from Unity, and restored when the route is popped",
                                     textAlign: TextAlign.center,
                                   ),
                                 ],
                               ),
                             )
-                          );
-                        },
-                        child: const Text("Open dialog"),
+                            );
+                          },
+                          child: const Text("Open dialog", textAlign: TextAlign.center),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              )
-            ],
-          ),
-        );
-      },
+                  ],
+                )
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 
@@ -221,40 +202,4 @@ class UnityScreen extends StatelessWidget {
       _fixedLocaleNumberFormatter.format(rotationSpeed),
     );
   }
-}
-
-
-class AppState with ChangeNotifier {
-  bool? isUnityArSupportedOnDevice;
-  bool isArSceneActive = false;
-  double rotationSpeed = 30;
-  int numberOfTaps = 0;
-
-  AppState();
-
-  void update({
-    bool? isUnityArSupportedOnDevice,
-    bool? isArSceneActive,
-    double? rotationSpeed,
-    int? numberOfTaps,
-  }) {
-    this.isUnityArSupportedOnDevice = isUnityArSupportedOnDevice ?? this.isUnityArSupportedOnDevice;
-    this.isArSceneActive = isArSceneActive ?? this.isArSceneActive;
-    this.rotationSpeed = rotationSpeed ?? this.rotationSpeed;
-    this.numberOfTaps = numberOfTaps ?? this.numberOfTaps;
-    notifyListeners();
-  }
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is AppState &&
-          runtimeType == other.runtimeType &&
-          isUnityArSupportedOnDevice == other.isUnityArSupportedOnDevice &&
-          isArSceneActive == other.isArSceneActive &&
-          rotationSpeed == other.rotationSpeed &&
-          numberOfTaps == other.numberOfTaps;
-
-  @override
-  int get hashCode => isUnityArSupportedOnDevice.hashCode ^ isArSceneActive.hashCode ^ rotationSpeed.hashCode ^ numberOfTaps.hashCode;
 }
