3e39e4cc16f0581f51cf812216f8e7f4677bdf72
James <no-reply@jamesncl.github.com>
2023-09-14 21:05:53 +0100

    Rename root platform dart file for consistency

---
+++ flutter_embed_unity_2022_3_ios/lib/flutter_embed_unity_2022_3_ios.dart
@@ -0,0 +1,4 @@
+
+// Nothing is needed here. This platform plugin uses the Flutter Method Channel
+// mechanism from the public-facing package, so the only thing that needs
+// implementing is the native side
