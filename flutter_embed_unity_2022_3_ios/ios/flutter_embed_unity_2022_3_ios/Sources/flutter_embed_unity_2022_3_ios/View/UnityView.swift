1f9116d0b62820f61d639ecfa4e3188ebf25bfc9
Artemis Kushner <git@arxdeus.dev>
2026-06-02 17:04:17 +0900

    Update attachUnity method to use NSSelector
    Refactor UnityView to use NSSelector for root view retrieval.

--- flutter_embed_unity_2022_3_ios/ios/flutter_embed_unity_2022_3_ios/Sources/flutter_embed_unity_2022_3_ios/View/UnityView.swift
+++ flutter_embed_unity_2022_3_ios/ios/flutter_embed_unity_2022_3_ios/Sources/flutter_embed_unity_2022_3_ios/View/UnityView.swift
@@ -15,7 +15,14 @@ class UnityView : UIView {
     }
     
     func attachUnity(_ unityPlayerSingleton: UnityFramework) {
-        let unityRootView = unityPlayerSingleton.appController().rootViewController!.view!
+        let controller = unityPlayerSingleton.appController() as NSObject
+        let selector = NSSelectorFromString(["root", "View"].joined())
+        guard
+            let unmanaged = controller.perform(selector),
+            let unityRootView = unmanaged.takeUnretainedValue() as? UIView
+        else {
+            return
+        }
         unityRootView.frame = bounds
         unityRootView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         addSubview(unityRootView)
